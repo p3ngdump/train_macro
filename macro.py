feb0546a34e62fbfab9fa4be5791b7960b6f1688
p3ngdump <opnkj67@naver.com>
2018-11-23 12:56:48 +0900

    Update macro.py

--- macro.py
+++ macro.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -6,21 +6,24 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
+start_station = "수원" # input start station
+end_station = "신창" # input end station
+
+print "[+] Input Your Korail_ID"
+korail_id = str(input())
+print "[+] Input Yoru Password"
+korail_password = str(input())
+
 driver = webdriver.Chrome("D:\\chromedriver.exe")
 
 driver.implicitly_wait(3)
 
 driver.get("http://www.letskorail.com/korail/com/login.do")
 
-driver.find_element_by_id("txtMember").send_keys("ID")
-driver.find_element_by_id("txtPwd").send_keys("PW")
+driver.find_element_by_id("txtMember").send_keys(korail_id)
+driver.find_element_by_id("txtPwd").send_keys(korail_password)
 
 driver.find_element_by_xpath("//*[@id=\"loginDisplay1\"]/ul/li[3]/a/img").click()
-
-
-
-start_station = "출발역"
-end_station = "도착역"
 
 driver.get("http://www.letskorail.com/ebizprd/EbizPrdTicketpr21100W_pr21110.do")
 
@@ -34,19 +37,18 @@ driver.find_element_by_name("txtGoEnd").send_keys(end_station.decode('utf-8'))
 
 # month
 select = Select(driver.find_element_by_id("s_month"))
-select.select_by_value("11") # select month
+select.select_by_value("11") # input month
 
 # date
 select2 = Select(driver.find_element_by_id("s_day"))
-select2.select_by_value("24") # select date
+select2.select_by_value("24") # input date
 
 # time
 select3 = Select(driver.find_element_by_id("s_hour"))
-select3.select_by_value("00") # select time
+select3.select_by_value("00") # input time
 
 driver.find_element_by_xpath("//*[@id=\"center\"]/form/div/p/a/img").click()
 
-# book loop
 while(1):
 	try:
 		value = driver.find_element_by_xpath("//*[@id=\"tableResult\"]/tbody/tr[2]/td[6]/a[1]/img")
